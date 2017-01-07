8322356bedaccc866d8a0cc7ed5648e970bd9456
dyego <dyego9444@hotmail.com>
2017-01-07 19:18:47 -0200

    inital files

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var express = require('express')
 var app = express()
+app.set('view engine', 'jade');
+
+
+var router = express.Router()
+
+//http interceptor
+router.use(function(req, res, next) {
+    next()
+});
+
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.render('index.jade')
 })
+
+
+router.get('/users', function(req, res){
+    res.send('Hello users')
+})
+
+app.use('/api', router)
+
 
 app.listen(3000)
