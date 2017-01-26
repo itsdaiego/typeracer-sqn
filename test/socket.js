83f62c6e200abddbceb0aa9497f017a71fdccd6c
Dyego Rodrigo <dyego9444@hotmail.com>
2017-01-25 23:16:32 -0200

    adding a hello world test using mocha

---
+++ test/socket.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+describe('connection', function() {
+    it('should return true', function() {
+        assert.equal(1, 1)
+    });
+    it('should return false', function() {
+        assert.equal(1, 0)
+    });
+});
+
