414cbfd42793f272e9391ec6797df24f3605ca7b
Dyego Rodrigo <dyego9444@hotmail.com>
2017-01-28 21:50:01 -0200

    creating basic structure to write tests

--- test/socket.js
+++ test/socket.js
@@ -1,10 +1,23 @@
-var assert = require('assert');
-describe('connection', function() {
+var should = require('should');
+var io = require('socket.io-client');
+
+var socketURL = 'http://0.0.0.0:5000';
+
+var options ={
+  transports: ['websocket'],
+  'force new connection': true
+};
+
+var chatUser1 = {'name':'Tom'};
+var chatUser2 = {'name':'Sally'};
+var chatUser3 = {'name':'Dana'};
+
+describe("Chat Server",function(){
     it('should return true', function() {
-        assert.equal(1, 1)
+        should(1).be.exactly(1)
     });
     it('should return false', function() {
-        assert.equal(1, 0)
+        should(1).be.exactly(0)
     });
-});
 
+});
