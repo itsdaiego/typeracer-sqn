15371c9d584efa433a2353c4d48d8bad94cc5e20
Dyego Rodrigo <dyego9444@hotmail.com>
2017-01-29 21:37:52 -0200

    added first test using mocha and chai

--- test/socket.js
+++ test/socket.js
@@ -1,23 +1,34 @@
-var should = require('should');
+var expect = require('chai').expect
 var io = require('socket.io-client');
 
-var socketURL = 'http://0.0.0.0:5000';
+var socketURL = 'http://0.0.0.0:3000';
 
-var options ={
-  transports: ['websocket'],
-  'force new connection': true
-};
+var roomData;
 
-var chatUser1 = {'name':'Tom'};
-var chatUser2 = {'name':'Sally'};
-var chatUser3 = {'name':'Dana'};
-
-describe("Chat Server",function(){
-    it('should return true', function() {
-        should(1).be.exactly(1)
-    });
-    it('should return false', function() {
-        should(1).be.exactly(0)
+describe("New Connection",function(){
+    beforeEach(function (done) {
+        server = require('../app').server;
+        done();
     });
 
+    it('should emit refreshCurrentUsers event to the client containing a user with name property equals to max and score equals to 0', function(done) {
+        roomData = {
+            roomname: 'supimpa',
+            username: 'max'
+        } 
+        var client = io.connect("http://localhost:3000");
+
+        client.on("connect", function () {
+            client.on("refreshCurrentUsers", function (users) {
+                for(var prop in users){
+                    expect(users[prop].score).to.equal(0);
+                    expect(users[prop].name).to.equal('max');
+                }
+                done();
+                client.disconnect();
+            });
+
+            client.send(roomData);
+        });
+    });
 });
