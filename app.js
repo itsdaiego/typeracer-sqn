7579723fcc09ca731567c349009955fce24d0ac7
Dyego Rodrigo <dyego9444@gmail.com>
2017-02-07 00:24:23 -0200

    testing userJoined event

--- app.js
+++ app.js
@@ -1,130 +1,132 @@
-var express = require('express');
-var socket = require('socket.io');
+"use strict"
 
-var app = express();
-var server = app.listen(3000);
+var express = require('express')
+var socket = require('socket.io')
 
-var io = socket(server);
+var app = express()
+var server = app.listen(3000)
 
-var sentences = require('./public/sentences.js');
-var gameModel = require('./models/game.js');
-var roomModel = require('./models/room.js');
+var io = socket(server)
 
-app.set('view engine', 'jade');
-app.use(express.static(__dirname + '/views'));
+var sentences = require('./public/sentences.js')
+var gameModel = require('./models/game.js')
+var roomModel = require('./models/room.js')
+
+app.set('view engine', 'jade')
+app.use(express.static(__dirname + '/views'))
 
 app.get('/', function(re, res){
-    res.render('index');
-});
+    res.render('index')
+})
 
 app.get('/room/:roomname/status', function(req, res){
-    var currentRoom = io.sockets.adapter.rooms[req.params.roomname] ? io.sockets.adapter.rooms[req.params.roomname] : undefined;
-    var roomInfo = {};
+    var currentRoom = io.sockets.adapter.rooms[req.params.roomname] ? io.sockets.adapter.rooms[req.params.roomname] : undefined
+    var roomInfo = {}
     if(!currentRoom){
-        currentRoom = "Room does not exist!";
-        res.send(currentRoom);
+        currentRoom = "Room does not exist!"
+        res.send(currentRoom)
     }
     else{
-        roomInfo.active_users = roomModel.getRoomActiveUsers(currentRoom);
-        roomInfo.keystrokes = currentRoom.totalKeystrokes;
-        roomInfo.active_since = roomModel.getSecondsSinceRoomWasCreated(currentRoom);
-        var meanScore = roomModel.getMeanScore(currentRoom);
-        roomInfo.below_mean = roomModel.getBelowMeanUsers(meanScore, currentRoom);
-        roomInfo.ranking = roomModel.getUsersRanking(currentRoom);
-        roomInfo.last_minute_lead = currentRoom.finalWinner.username;
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(roomInfo));
+        roomInfo.active_users = roomModel.getRoomActiveUsers(currentRoom)
+        roomInfo.keystrokes = currentRoom.totalKeystrokes
+        roomInfo.active_since = roomModel.getSecondsSinceRoomWasCreated(currentRoom)
+        var meanScore = roomModel.getMeanScore(currentRoom)
+        roomInfo.below_mean = roomModel.getBelowMeanUsers(meanScore, currentRoom)
+        roomInfo.ranking = roomModel.getUsersRanking(currentRoom)
+        roomInfo.last_minute_lead = currentRoom.finalWinner.username
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify(roomInfo))
     }
-});
+})
 
 app.get('/room/:roomname/user/:username', function(req, res){
     var room = {
         username: req.params.username,
         roomname: req.params.roomname
-    };
-    res.render('room', room);
-});
+    }
+    res.render('room', room)
+})
 
-var sentences = sentences.getEnglishSentences();
+var sentences = sentences.getEnglishSentences()
 
 io.sockets.on('connection', function (socket) {
-    var currentRoom;
+    var currentRoom
     socket.on('message', function(roomData){
-        gameModel.setConnectionProperties(socket, currentRoom, roomData);
+        gameModel.setConnectionProperties(socket, currentRoom, roomData)
 
-        socket.join(socket.room);
+        socket.join(socket.room)
 
 
-        currentRoom = io.sockets.adapter.rooms[roomData.roomname];
-        gameModel.setCurrentRoomProperties(currentRoom, socket);
+        currentRoom = io.sockets.adapter.rooms[roomData.roomname]
+        gameModel.setCurrentRoomProperties(currentRoom, socket)
         
-        io.sockets.in(socket.room).emit('refreshCurrentUsers', currentRoom.users);
+        io.sockets.in(socket.room).emit('refreshCurrentUsers', currentRoom.users)
         socket.broadcast.to(socket.room).emit('userJoined', {
             username: socket.username,
             roomname: socket.room
-        });
+        })
 
-    });
+    })
     
     socket.on('userReady', function(roomData){
-        currentRoom.usersReady++;
+        currentRoom.usersReady++
         if(currentRoom.usersReady == currentRoom.length){
-            io.sockets.in(socket.room).emit('startGame');
-            io.sockets.in(socket.room).emit('newSentence', sentences[socket.sentenceCounter]);
+            io.sockets.in(socket.room).emit('startGame')
+            io.sockets.in(socket.room).emit('newSentence', sentences[socket.sentenceCounter])
 
-            io.sockets.in(socket.room).emit('timeRemaining', currentRoom.gameDuration);
+            io.sockets.in(socket.room).emit('timeRemaining', currentRoom.gameDuration)
             var intervalId = setInterval(function(){
-                currentRoom.roundTimeCounter++;
-                currentRoom.gameDuration--;
+                currentRoom.roundTimeCounter++
+                currentRoom.gameDuration--
                 if(currentRoom.roundTime == currentRoom.roundTimeCounter){
-                    roomModel.resetRoomCounters(currentRoom);
+                    roomModel.resetRoomCounters(currentRoom)
 
-                    roomModel.setTotalKeystrokes(currentRoom);
-                    gameModel.setFinalWinner(currentRoom);
-                    io.sockets.in(socket.room).emit('roundFinished');
+                    roomModel.setTotalKeystrokes(currentRoom)
+                    gameModel.setFinalWinner(currentRoom)
+                    io.sockets.in(socket.room).emit('roundFinished')
 
                 }
                 if(currentRoom.gameDuration <= 0){
-                    clearInterval(intervalId);
-                    socket.emit('gameFinished', currentRoom.finalWinner);
+                    clearInterval(intervalId)
+                    socket.emit('gameFinished', currentRoom.finalWinner)
                 }
                 else{
-                    io.sockets.in(socket.room).emit('timeRemaining', currentRoom.gameDuration);
+                    io.sockets.in(socket.room).emit('timeRemaining', currentRoom.gameDuration)
                 }
         
-            }, 1000);
+            }, 1000)
         }
-    });
+    })
 
     socket.on('resetScore', function(username){
-        socket.score = 0;
-    });
+        socket.score = 0
+    })
     
     socket.on('sendPlayerScore', function(username){
-        socket.score++;
-        currentRoom.users[socket.id].score = socket.score;
-        gameModel.setCurrentWinner(currentRoom, socket);
-        gameModel.updateScore(io, socket);
-    });
+        socket.score++
+        currentRoom.users[socket.id].score = socket.score
+        gameModel.setCurrentWinner(currentRoom, socket)
+        gameModel.updateScore(io, socket)
+    })
 
     socket.on('sentenceFinished', function(){
-        socket.sentenceCounter++;
+        socket.sentenceCounter++
         if(socket.sentenceCounter == sentences.length){
-            socket.sentenceCounter = 0;
+            socket.sentenceCounter = 0
         }
-        socket.emit('newSentence', sentences[socket.sentenceCounter]);
-    });
+        socket.emit('newSentence', sentences[socket.sentenceCounter])
+    })
 
     socket.on('disconnect', function(){
         if(socket.id && currentRoom){
-            currentRoom.usersReady--;
-            socket.leave(socket.room);
-            delete currentRoom.users[socket.id];
-            io.sockets.in(socket.room).emit('userLeft', socket.username, currentRoom.users);
-            io.sockets.in(socket.room).emit('refreshCurrentUsers', currentRoom.users);
+            currentRoom.usersReady--
+            socket.leave(socket.room)
+            delete currentRoom.users[socket.id]
+            io.sockets.in(socket.room).emit('userLeft', socket.username, currentRoom.users)
+            io.sockets.in(socket.room).emit('refreshCurrentUsers', currentRoom.users)
 
         }
-    });
+    })
 
-});
+})
 
--- test/socket.js
+++ test/socket.js
@@ -1,9 +1,10 @@
+"use strict"
 var expect = require('chai').expect
-var io = require('socket.io-client');
+var io = require('socket.io-client')
 
-var socketURL = 'http://0.0.0.0:3000';
+var socketURL = 'http://0.0.0.0:3000'
 
-var roomData;
+var roomData
 
 describe('New Connection',function(){
     beforeEach(function (done) {
@@ -11,54 +12,70 @@ describe('New Connection',function(){
             roomname: 'supimpa',
             username: 'max'
         } 
-        server = require('../app').server;
-        done();
-    });
+        var server = require('../app').server
+        done()
+    })
+
 
     it('should emit refreshCurrentUsers event to the client containing a user with name property equals to max and score equals to 0', function(done) {
-        var client = io.connect('http://localhost:3000');
+        var client = io.connect('http://localhost:3000')
 
         client.on('connect', function () {
             client.on('refreshCurrentUsers', function (users) {
                 for(var prop in users){
-                    expect(users[prop].score).to.equal(0);
-                    expect(users[prop].name).to.equal('max');
+                    expect(users[prop].score).to.equal(0)
+                    expect(users[prop].name).to.equal('max')
                 }
-                done();
-                client.disconnect();
-            });
-
-            client.send(roomData);
-        });
-    });
+                done()
+                client.disconnect()
+            })
+        })
+        client.send(roomData)
+    })
 
     it('should fire userJoined event to all users but the user the has just joined', function(done){
-        var client = io.connect('http://localhost:3000');
-        var client2 = io.connect('http://localhost:3000');
-        var client3 = io.connect('http://localhost:3000');
+        var client = io.connect('http://localhost:3000')
+        var client2 = io.connect('http://localhost:3000')
+        var client3 = io.connect('http://localhost:3000')
         client.on('connect', function(){
-            client2.send(roomData);
+            client2.send(roomData)
             client2.on('connect', function(){
                 client.on('userJoined', function(data){
-                    expect(data.username).to.equal(roomData.username);
-                    expect(data.roomname).to.equal(roomData.roomname);
-                    client2.disconnect();
-                });
-            });
+                    expect(data.username).to.equal(roomData.username)
+                    expect(data.roomname).to.equal(roomData.roomname)
+                })
+            })
 
-            client3.send(roomData);
+            client3.send(roomData)
             client3.on('connect', function(){
                 client2.on('userJoined', function(data){
-                    expect(data.username).to.equal(roomData.username);
-                    expect(data.roomname).to.equal(roomData.roomname);
-                    client2.disconnect();
-                    client.disconnect();
+                    expect(data.username).to.equal(roomData.username)
+                    expect(data.roomname).to.equal(roomData.roomname)
                 })
-            });
-        });
-        client.send(roomData);
-        done();
+            })
+        })
+        client.send(roomData)
+        done()
+        client.disconnect()
+        client2.disconnect()
+        client3.disconnect()
+    })
 
+    it('should fire userReady event only when the number of players are equal to the ready players number', function(done){
+        var client = io.connect('http://localhost:3000')
+        
+        client.on('connect', function(){
+            console.log('im here')
+            // client.emit('userReady', roomData) 
 
-    });
-});
+            // client.on('startGame', function(data){
+            //     expect(data).to.equal(1)
+            // })
+            client.send(roomData)
+            done()
+            client.disconnect()
+        })
+
+    })
+
+})
