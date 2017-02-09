9aa03cd9b7111f3a6d62d41dd6075f5f4373770c
Dyego Rodrigo <dyego9444@gmail.com>
2017-02-08 23:34:38 -0200

    encapsulating game logic on socket.js file

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ var express = require('express')
 var socket = require('socket.io')
 
 var app = express()
-var server = app.listen(3000)
-
-var io = require('./socket.js').startServer(server)
-var sentences = require('./public/sentences.js')
+var server = app.listen(3000) 
 var gameModel = require('./models/game.js')
 var roomModel = require('./models/room.js')
+var sentences = require('./public/sentences.js')
+var io = require('./socket.js').startServer(server, roomModel, gameModel, sentences)
 
 app.set('view engine', 'jade')
 app.use(express.static(__dirname + '/views'))
@@ -46,86 +45,4 @@ app.get('/room/:roomname/user/:username', function(req, res){
     res.render('room', room)
 })
 
-var sentences = sentences.getEnglishSentences()
-
-io.sockets.on('connection', function (socket) {
-    var currentRoom
-    socket.on('message', function(roomData){
-        gameModel.setConnectionProperties(socket, currentRoom, roomData)
-
-        socket.join(socket.room)
-
-
-        currentRoom = io.sockets.adapter.rooms[roomData.roomname]
-        gameModel.setCurrentRoomProperties(currentRoom, socket)
-        
-        io.sockets.in(socket.room).emit('refreshCurrentUsers', currentRoom.users)
-        socket.broadcast.to(socket.room).emit('userJoined', {
-            username: socket.username,
-            roomname: socket.room
-        })
-
-    })
-    
-    socket.on('userReady', function(roomData){
-        currentRoom.usersReady++
-        if(currentRoom.usersReady == currentRoom.length){
-            io.sockets.in(socket.room).emit('startGame')
-            io.sockets.in(socket.room).emit('newSentence', sentences[socket.sentenceCounter])
-
-            io.sockets.in(socket.room).emit('timeRemaining', currentRoom.gameDuration)
-            var intervalId = setInterval(function(){
-                currentRoom.roundTimeCounter++
-                currentRoom.gameDuration--
-                if(currentRoom.roundTime == currentRoom.roundTimeCounter){
-                    roomModel.resetRoomCounters(currentRoom)
-
-                    roomModel.setTotalKeystrokes(currentRoom)
-                    gameModel.setFinalWinner(currentRoom)
-                    io.sockets.in(socket.room).emit('roundFinished')
-
-                }
-                if(currentRoom.gameDuration <= 0){
-                    clearInterval(intervalId)
-                    socket.emit('gameFinished', currentRoom.finalWinner)
-                }
-                else{
-                    io.sockets.in(socket.room).emit('timeRemaining', currentRoom.gameDuration)
-                }
-        
-            }, 1000)
-        }
-    })
-
-    socket.on('resetScore', function(username){
-        socket.score = 0
-    })
-    
-    socket.on('sendPlayerScore', function(username){
-        socket.score++
-        currentRoom.users[socket.id].score = socket.score
-        gameModel.setCurrentWinner(currentRoom, socket)
-        gameModel.updateScore(io, socket)
-    })
-
-    socket.on('sentenceFinished', function(){
-        socket.sentenceCounter++
-        if(socket.sentenceCounter == sentences.length){
-            socket.sentenceCounter = 0
-        }
-        socket.emit('newSentence', sentences[socket.sentenceCounter])
-    })
-
-    socket.on('disconnect', function(){
-        if(socket.id && currentRoom){
-            currentRoom.usersReady--
-            socket.leave(socket.room)
-            delete currentRoom.users[socket.id]
-            io.sockets.in(socket.room).emit('userLeft', socket.username, currentRoom.users)
-            io.sockets.in(socket.room).emit('refreshCurrentUsers', currentRoom.users)
-
-        }
-    })
-
-})
 
